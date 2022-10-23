306c7feebc19e159a4d838c093ffb820fc738cc4
Kit <34525535+Kit-Law@users.noreply.github.com>
2022-10-23 16:26:25 +0100

    feat(FeatureExtractor) Created a zero-crossing rate function

--- Src/Classifier/FeatureExtractor.py
+++ Src/Classifier/FeatureExtractor.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from scipy.io import wavfile
+import librosa
 
-def plot_fft(filename):                                                                     # plot the frequencies of a wav file
+def plot_fft(filename) :                                                                     # plot the frequencies of a wav file
     samplerate, transform = wavfile.read(filename)
 
     plt.plot(np.fft.rfft(transform))
@@ -12,16 +13,24 @@ def plot_fft(filename):                                                         
     plt.ylabel("Hz")
     plt.show()
 
-def calculate_spectral_centroid(data, sampleRate):
+def calculate_spectral_centroid(data, sampleRate) :
     magnitudes = np.abs(np.fft.rfft(data))                                                  # magnitudes of positive frequencies
     length = len(data)
     freqs = np.abs(np.fft.fftfreq(length, 1.0/sampleRate)[:length//2+1])                    # positive frequencies
     sums = np.sum(magnitudes)
     return 0 if sums == 0 else np.sum(magnitudes*freqs) / sums                                    # return weighted mean
 
-def wav_to_spectral_centroid(fileName, frameSize):
+def wav_to_spectral_centroid(fileName, frameSize) :
     sampleRate, spectralDencity = wavfile.read(fileName)
     length = spectralDencity.shape[0] / sampleRate
 
     frames = [spectralDencity[i:i+(frameSize)] for i in range(0, len(spectralDencity), (frameSize))]   # group spectralDencity into frames
     return [calculate_spectral_centroid(frame, sampleRate) for frame in frames]             # return list of spectral centroids
+
+def wav_to_ZCR(fileName, frameSize) :
+    _, spectralDencity = wavfile.read(fileName)
+    zeroCrossings = np.nonzero(np.diff(spectralDencity > 0))[0]
+    zcr = [0] * int(len(spectralDencity) / frameSize + 1)
+    for point in zeroCrossings :
+        zcr[int(point / frameSize)] += 1
+    return zcr
--- Src/Classifier/FeatureExtractorTest.py
+++ Src/Classifier/FeatureExtractorTest.py
@@ -9,17 +9,23 @@ frameSize = 500
 
 #plot_fft('../../Data/5-251489-A-24.wav')
 
-centroids = fe.wav_to_spectral_centroid('../../Data/5-251489-A-24.wav', frameSize)
-centroidsTwo = fe.wav_to_spectral_centroid('../../Data/5-263902-A-36.wav', frameSize)
+centroids = fe.wav_to_spectral_centroid('../../Data/airplane/1-11687-A-47.wav', frameSize)
+centroidsTwo = fe.wav_to_spectral_centroid('../../Data/dog/1-30226-A-0.wav', frameSize)
 centroidsThree = fe.wav_to_spectral_centroid('../../Data/crying_baby/2-66637-B-20.wav', frameSize)
 
-testWav, sr = librosa.load('../../Data/5-251489-A-24.wav')
-test = librosa.feature.spectral_centroid(y=testWav, sr=sr, n_fft=frameSize, hop_length=500)[0]
+zeroCrossingRate = fe.wav_to_ZCR('../../Data/airplane/1-11687-A-47.wav', 2048)
 
-plt.plot(centroids, color='r')
-plt.plot(centroidsTwo, color='b')
-plt.plot(centroidsThree, color='g')
-plt.plot
+testWav, sr = librosa.load('../../Data/airplane/1-11687-A-47.wav')
+testCentroids = librosa.feature.spectral_centroid(y=testWav, sr=sr, n_fft=frameSize, hop_length=500)[0]
+testZCR = librosa.feature.zero_crossing_rate(y=testWav)[0]
+
+plt.plot(zeroCrossingRate, color='b')
+plt.show()
+
+plt.plot(testZCR, color='r')
+#plt.plot(centroids, color='r')
+#plt.plot(centroidsTwo, color='b')
+#plt.plot(centroidsThree, color='g')
 plt.legend()
 plt.xlabel("Centroids")
 plt.ylabel("Spectral Rate")
