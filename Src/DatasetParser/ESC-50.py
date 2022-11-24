15a9a96bb9f28cafa72a965d4c001a117f39bb2c
Kit <34525535+Kit-Law@users.noreply.github.com>
2022-11-24 16:00:55 +0000

    feat(Dataset) Split ESC-50 into predefined folds

--- Src/DatasetParser/ESC-50.py
+++ Src/DatasetParser/ESC-50.py
@@ -5,10 +5,15 @@ def read_csv_label_data(csvFileName, dir) :
     with open(csvFileName, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            relocate_file(dir, row['filename'], row['category'])
+            relocate_file_with_fold(dir, row['filename'], row['category'], row['fold'])
 
 def relocate_file(dir, filename, label) :
     os.makedirs(dir + "/" + label, exist_ok=True)
     os.rename(dir + "/audio/" + filename, dir + "/" + label + "/" + filename)
 
-read_csv_label_data("../../Data/ESC-50/esc50.csv", "../../Data/ESC-50")
+def relocate_file_with_fold(dir, filename, label, fold) :
+    os.makedirs(dir + "/fold" + fold, exist_ok=True)
+    os.makedirs(dir + "/fold" + fold + "/" + label, exist_ok=True)
+    os.rename(dir + "/" + filename, dir + "/fold" + fold + "/" + label + "/" + filename)
+
+read_csv_label_data("U:/GDP/ML Testing/Low-Power-Sound-Recognition-Classification/Data/ESC-50-master/meta/esc50.csv", "U:/GDP/ML Testing/Low-Power-Sound-Recognition-Classification/Data/ESC-50-master/audio")
